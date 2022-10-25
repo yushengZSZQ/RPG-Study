b67e2215c2dfd60da62b1dfa969ec3e8d81dfe96
yushengZSZQ <109350056+yushengZSZQ@users.noreply.github.com>
2022-10-25 14:59:37 +0800

    Create Asset

---
+++ Assets/Elements/FlatKit/Shaders/Editor/Tooltips.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace FlatKit.StylizedSurface {
+public static class Tooltips {
+    public static Dictionary<String, String> map = new Dictionary<string, string> {
+        {"Color Shaded", "The dark part of cel shaded objects."},
+        {"Center X", "World-space X coordinate of the middle gradient point."},
+    };
+}
+}
--- UI_JoyStick.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UI_JoyStick : MonoBehaviour
-{
-    public CommonJoyBtn commonBtn;
-    public Vector3 Dir => (commonBtn.Dir);
-}
